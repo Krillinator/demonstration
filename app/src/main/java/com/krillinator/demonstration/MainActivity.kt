22047b1d71d1e0edb5362138ca7a9dbdac2d15f6
Kristoffer Johansson <kristoffer.johansson@gaddr.com>
2022-04-06 13:40:13 +0200

    initial commit

---
+++ app/src/main/java/com/krillinator/demonstration/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.krillinator.demonstration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        // RecyclerView Adapter - Do next
+        /* TODO - RecyclerView Implementation */
+
+        
+
+    }
+}
